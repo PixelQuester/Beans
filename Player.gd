a52030f735004afbf052f67cbadd027dbcb954bd
AndrewY <andrewyoun1017@gmail.com>
2024-01-09 16:21:23 -0500

    Simple attacking
    Bug- Not attacking when moving?

--- Player.gd
+++ Player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var speed : float = 600.0
 
+var is_attacking := false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,8 +19,17 @@ func _physics_process(delta):
 	var xmove = Input.get_axis("ui_left","ui_right")
 	var direction = Vector2(xmove, ymove).normalized()
 	
+	if is_attacking:
+		$Weapon.show()
+	else:
+		$Weapon.rotation = (get_global_mouse_position()-global_position).angle() + PI/4
+		$Weapon.hide()
+	
 	global_position += direction * delta * speed
 	
 func _input(event):
-	if event.is_action("ui_accept"):
+	if event.is_action_pressed("ui_accept") and is_attacking == false:
 		print("Beans")
+		is_attacking = true
+		await get_tree().create_timer(.5).timeout
+		is_attacking = false
